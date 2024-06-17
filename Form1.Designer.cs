f33445e8c01a06a385781571f33a6b9b3cb057b0
Marco Lima <159477189+marco-lima-1@users.noreply.github.com>
2024-06-17 14:59:34 -0300

    Project up

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Projeto01
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCPF = new System.Windows.Forms.MaskedTextBox();
+            this.txtTel = new System.Windows.Forms.MaskedTextBox();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(15, 63);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(84, 13);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome completo:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CPF:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Celular:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Enabled = false;
+            this.txtNome.Location = new System.Drawing.Point(122, 60);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(322, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Enabled = false;
+            this.txtCPF.Location = new System.Drawing.Point(122, 87);
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(125, 20);
+            this.txtCPF.TabIndex = 4;
+            // 
+            // txtTel
+            // 
+            this.txtTel.Enabled = false;
+            this.txtTel.Location = new System.Drawing.Point(122, 113);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(125, 20);
+            this.txtTel.TabIndex = 5;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(110, 198);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(75, 23);
+            this.btnNovo.TabIndex = 6;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Enabled = false;
+            this.btnSalvar.Location = new System.Drawing.Point(191, 198);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 7;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(187, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(318, 22);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "TELA DE CADASTRO DE CLIENTES";
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Enabled = false;
+            this.btnExcluir.Location = new System.Drawing.Point(272, 198);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 9;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Enabled = false;
+            this.btnCancelar.Location = new System.Drawing.Point(353, 198);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 10;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.txtCancelar_Click);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 396);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.txtCPF);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblNome);
+            this.Name = "FrmPrincipal";
+            this.Text = "Principal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.MaskedTextBox txtCPF;
+        private System.Windows.Forms.MaskedTextBox txtTel;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Runtime.Remoting.Contexts;
+
+namespace Projeto01
+{
+    public partial class FrmPrincipal : Form
+    {
+
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            HabilitarCampos();
+            txtNome.Focus();
+            HabilitarBotoes();
+            btnNovo.Enabled = false;
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+            DesabilitarCampos();
+            DesabilitarBotoes();
+            btnNovo.Enabled = true;
+
+            //criamos a string de conexao
+            SqlConnection conn = new SqlConnection("Data Source = OGPC; Initial Catalog = aula; User Id=sa;Password=39465512");
+
+            //cria string de inserção sql
+            string sql = "INSERT INTO Cliente(id, nome, cpf, tel) VALUES (@id, @nome, @cpf, @tel)";
+            Random numeroID = new Random();
+            numeroID.Next();
+
+
+
+            try
+            {
+                //cria um objeto do tipo comando passando como parametro a string sql e conn
+                SqlCommand c = new SqlCommand(sql, conn);
+
+                //insere os dados da texBox no comando sql
+                c.Parameters.Add(new SqlParameter("@id", numeroID.Next()));
+                c.Parameters.Add(new SqlParameter("@nome", this.txtNome.Text));
+                c.Parameters.Add(new SqlParameter("@cpf", this.txtCPF.Text));
+                c.Parameters.Add(new SqlParameter("@tel", this.txtTel.Text));
+
+                //abrimos a conexao com banco de dados
+                conn.Open();
+
+                //executa o comando sql no banco de dados
+                c.BeginExecuteNonQuery();
+
+                //Fechamos a conexao
+                conn.Close();
+
+                //aviso que deu certo
+                MessageBox.Show("Cadastro concluído com êxito");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ocorreu o erro: " + ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+            DesabilitarCampos();
+            DesabilitarBotoes();
+            btnNovo.Enabled = true;
+        }
+
+        private void txtCancelar_Click(object sender, EventArgs e)
+        {
+            DesabilitarBotoes();
+            btnNovo.Enabled = true;
+            LimparCampos();
+            DesabilitarCampos();
+        }
+
+        //metodo desabilita botoes
+        private void DesabilitarBotoes()
+        {
+            btnNovo.Enabled = false;
+            btnSalvar.Enabled = false;
+            btnExcluir.Enabled = false;
+            btnCancelar.Enabled = false;
+        }
+
+        //metodo habilita botoes
+        private void HabilitarBotoes()
+        {
+            btnNovo.Enabled = true;
+            btnSalvar.Enabled = true;
+            btnExcluir.Enabled = true;
+            btnCancelar.Enabled = true;
+        }
+        //metodo limpa textos
+        private void LimparCampos()
+        {
+            txtNome.Clear();
+            txtCPF.Clear();
+            txtTel.Clear();
+        }
+        //metodo desabilita textos
+        private void DesabilitarCampos()
+        {
+            txtNome.Enabled = false;
+            txtCPF.Enabled = false;
+            txtTel.Enabled = false;
+        }
+        //metodo habilita textos
+        private void HabilitarCampos()
+        {
+            txtNome.Enabled = true;
+            txtCPF.Enabled = true;
+            txtTel.Enabled = true;
+        }
+
+
+
+
+
+
+
+    }
+}
